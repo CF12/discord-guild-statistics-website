6cb274b294fa1ebac1b801fcc24d8a91d3bfd9e5
CF12 <brianx0011@gmail.com>
2017-12-25 21:43:25 -0500

    Worked on reworking API

--- src/server/server.js
+++ src/server/server.js
@@ -9,6 +9,7 @@ const DATA_PATH = path.join(__dirname, '..', '..', 'data')
 const PORT = 3001
 
 const db = low(new FileSync(path.join(DATA_PATH, 'db.json')))
+const router = express.Router()
 
 app.use(express.static(BUILD_PATH))
 app.set('views', BUILD_PATH)
@@ -17,10 +18,20 @@ app.set('view engine', 'html')
 
 app.use(express.json())
 
-app.get('/api/v1/state/playing', require('./services/api/v1/state/playing/sum').handler(db))
-
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
   res.render('index.html')
+})
+
+app.user('/api', router)
+
+router.route('/users')
+.get((req, res) => {
+  let handler = require('./services/api/v1/state/playing/sum').handler(db)
+
+  res.json({
+    date: Date.now(),
+    users: handler.getUsers()
+  })
 })
 
 app.listen(PORT)
